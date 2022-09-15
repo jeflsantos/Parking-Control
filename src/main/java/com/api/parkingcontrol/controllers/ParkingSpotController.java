7a243ec03fdae4124b6242bce6413c65dafc835e
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-09-15 16:32:25 -0300

    Projeto Parking Spot - Spring Boot
    Projeto Parking Spot - Spring Boot

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -77,14 +77,18 @@ public class ParkingSpotController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Parking Spot not found.");
         }
         var parkingSpotModel = new ParkingSpotModel();
-        parkingSpotModel.setParkingSpotNumber(parkingSpotDto.getParkingSpotNumber());
-        parkingSpotModel.setLicensePlateCar(parkingSpotDto.getLicensePlateCar());
-        parkingSpotModel.setModelCar(parkingSpotDto.getModelCar());
-        parkingSpotModel.setBrandCar(parkingSpotDto.getBrandCar());
-        parkingSpotModel.setColorCar(parkingSpotDto.getColorCar());
-        parkingSpotModel.setResponsibleName(parkingSpotDto.getResponsibleName());
-        parkingSpotModel.setApartment(parkingSpotDto.getApartment());
-        parkingSpotModel.setBlock(parkingSpotDto.getBlock());
+//        parkingSpotModel.setParkingSpotNumber(parkingSpotDto.getParkingSpotNumber());
+//        parkingSpotModel.setLicensePlateCar(parkingSpotDto.getLicensePlateCar());
+//        parkingSpotModel.setModelCar(parkingSpotDto.getModelCar());
+//        parkingSpotModel.setBrandCar(parkingSpotDto.getBrandCar());
+//        parkingSpotModel.setColorCar(parkingSpotDto.getColorCar());
+//        parkingSpotModel.setResponsibleName(parkingSpotDto.getResponsibleName());
+//        parkingSpotModel.setApartment(parkingSpotDto.getApartment());
+//        parkingSpotModel.setBlock(parkingSpotDto.getBlock());
+
+        BeanUtils.copyProperties(parkingSpotDto, parkingSpotModel);
+        parkingSpotModel.setId(parkingSpotModelOptional.get().getId());
+        parkingSpotModel.setRegistrationDate(parkingSpotModelOptional.get().getRegistrationDate());
 
         return ResponseEntity.status(HttpStatus.OK).body(parkingSpotService.save(parkingSpotModel));
     }
